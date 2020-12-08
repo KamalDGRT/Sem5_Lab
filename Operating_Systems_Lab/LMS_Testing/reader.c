16c7de13d3496f490c3356bb59ad30e55fcd0a84
Kamal Sharma <kamaldgrt@gmail.com>
2020-12-08 15:44:22 +0530

    update reader.c

--- Operating_Systems_Lab/LMS_Testing/reader.c
+++ Operating_Systems_Lab/LMS_Testing/reader.c
@@ -2,13 +2,13 @@
 #include<stdlib.h>
 
 int main() {
-    typedef int semphore;
-
-    semphore sread = 0, swrite = 0;
+    typedef int semaphore;
+    
+    semaphore sread = 0, swrite = 0;
     int ch, r = 0;
-
+    
     printf("Reader writer\n");
-
+    
     do {
         printf("Menu");
         printf("\n1.Read from file");
@@ -16,58 +16,59 @@ int main() {
         printf("\n3.Exit the reader");
         printf("\n4.Exit the writer");
         printf("\n5.Exit");
-
+        
         printf("\nEnter your choice:");
         scanf("%d", &ch);
-
+        if(ch < 5)
+        printf("%d", ch);
+        
         switch(ch) {
-            case 1: 
-                if(swrite == 0) {
-                    sread = 1;
-                    r += 1;
-                    printf("Reader %d reads\n", r);
-                } else {
-                    printf("Not possible\n");
-                }
+            case 1:
+            if(swrite == 0) {
+                sread = 1;
+                r += 1;
+                printf("\nReader %d reads\n", r);
+            } else {
+                printf("Not possible\n");
+            }
             break;
-
-            case 2: 
-                if(sread == 0 && swrite == 0) {
-                    swrite = 1;
-                    printf("Writer in progress\n");
-                } else if (swrite == 1) {
-                    printf("Writer writes the files\n");
-                } else if (sread == 1) {
-                    printf("Cannot write while reader reads the file\n");
-                }
-                break;
-
-            case 3: 
-                if(r != 0) {
-                    printf("The reader %d closes the file\n", r);
-                    r -= 1;
-                    if(r == 0) {
-                        sread = 0;
-                    } 
-                } else if (r == 0) {
-                    printf("Currently no readers access the file\n");
+            
+            case 2:
+            if(sread == 0 && swrite == 0) {
+                swrite = 1;
+                printf("\nwriter in progress\n");
+            } else if (swrite == 1) {
+                printf("Writer writes the files\n");
+            } else if (sread == 1) {
+                printf("Cannot write while reader reads the file\n");
+            }
+            break;
+            
+            case 3:
+            if(r != 0) {
+                printf("\nThe reader %d closes the file\n", r);
+                r -= 1;
+                if (r == 0) {
                     sread = 0;
                 }
+            } else if (r == 0) {
+                printf("Currently no readers access the file\n");
+                sread = 0;
+            }
             break;
-
-            case 4: 
-                if(swrite == 1) {
-                    printf("Writer closes the file\n");
-                    swrite = 0;
-                } else {
-                    printf("There is no writer in the file\n");
-                }
+            
+            case 4:
+            if(swrite == 1) {
+                printf("\nwriter close the file\n");
+                swrite = 0;
+            } else {
+                printf("There is no writer in the file\n");
+            }
             break;
-
+            
             case 5: exit(0);
         }
-    } while(ch < 6);
-
+    } while (ch < 6);
+    
     return 0;
 }
-
